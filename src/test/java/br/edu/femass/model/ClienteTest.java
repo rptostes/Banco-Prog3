57da7bb1b6d0029704eef93a7dbf2bff67b82592
natassjalessa <natassjalessa1@gmail.com>
2022-08-30 18:43:28 -0300

    Teste finalizado

--- src/test/java/br/edu/femass/model/ClienteTest.java
+++ src/test/java/br/edu/femass/model/ClienteTest.java
@@ -1,22 +1,30 @@
 package br.edu.femass.model;
 
+import br.edu.femass.utils.Cpf;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ClienteTest {
 
     @Test
     void criarClienteCpfValido() {
-
+        Assertions.assertTrue(Cpf.isCpf("93810535028"));
     }
 
     @Test
     void criarClienteCpfInvalido() {
-
+        Assertions.assertTrue(Cpf.isCpf("93810535029"));
     }
 
     @Test
     void criarContaGerandoNumero() {
-
+        Cliente cliente = new Cliente(
+                "Jose Silva",
+                "93810535028");
+        cliente.criarConta(1000D);
+        Conta conta = cliente.getContas().get(0);
+        Long esperado = 1L;
+        Assertions.assertEquals(esperado, conta.getNumero());
     }
 
 
--- src/test/java/br/edu/femass/model/ContaTest.java
+++ src/test/java/br/edu/femass/model/ContaTest.java
@@ -1,5 +1,6 @@
 package br.edu.femass.model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,22 +9,62 @@ class ContaTest {
 
     @Test
     void creditarValorConta() {
-
+        Cliente cliente = new Cliente(
+                "João da Silva",
+                "65006006064" );
+        cliente.criarConta(0D);
+        Conta conta = cliente.getContas().get(0);
+        conta.creditar(3000D);
+        Double esperado = 3000D;
+        Assertions.assertEquals(esperado, conta.getSaldo());
     }
 
     @Test
     void debitarValorContaComSaldo() {
-
+        Cliente cliente = new Cliente(
+                "João da Silva",
+                "65006006064" );
+        cliente.criarConta(0D);
+        Conta conta = cliente.getContas().get(0);
+        conta.creditar(1000D);
+        conta.debitar(500D);
+        Double esperado = 500D;
+        Assertions.assertEquals(esperado, conta.getSaldo());
     }
 
     @Test
     void debitarValorContaSemSaldo() {
-
+        Cliente cliente = new Cliente(
+                "João da Silva",
+                "65006006064" );
+        cliente.criarConta(0D);
+        Conta conta = cliente.getContas().get(0);
+        Double esperado = 0D;
+        try {
+            conta.debitar(1000D);
+        } catch (IllegalArgumentException ex) {
+            Assertions.assertEquals(esperado, conta.getSaldo());
+        }
     }
 
     @Test
     void validarHistoricoLancamentos() {
-
+        Cliente cliente = new Cliente(
+                "João da Silva",
+                "65006006064" );
+        cliente.criarConta(0D);
+        Conta conta = cliente.getContas().get(0);
+        try {
+            conta.creditar(2000D);
+            conta.debitar(500D);
+            conta.debitar(150D);
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("Saldo da conta: "
+                + conta.getSaldo());
+        System.out.println("Histórico de operações:\n"
+                + conta.getHistorico());
     }
 
 }
