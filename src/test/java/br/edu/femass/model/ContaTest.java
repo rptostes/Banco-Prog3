fe0dcaef3a1b099c8f860b74f158715596279432
José Vitor Façanha da Silva <jvitorfacanha@outlook.com>
2022-08-30 12:00:29 -0300

    Teste - Classe Conta

--- src/test/java/br/edu/femass/model/ContaTest.java
+++ src/test/java/br/edu/femass/model/ContaTest.java
@@ -1,24 +1,36 @@
 package br.edu.femass.model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class ContaTest {
 
     @Test
     void creditarValorConta() {
-
+        Conta conta = new Conta(10D);
+        conta.creditar(30D);
+        Assertions.assertNotNull(conta.getSaldo());
     }
 
     @Test
     void debitarValorContaComSaldo() {
+          Conta conta = new Conta (20D);
+          conta.creditar(20D);
+          conta.debitar(10D);
+
+
+        Assertions.assertTrue(conta.getSaldo() > 0, "É possivel realizar o saque nessa conta");
 
     }
 
     @Test
     void debitarValorContaSemSaldo() {
+        Conta conta = new Conta(30D);
+        conta.debitar(20D);
 
+        Assertions.assertTrue(conta.getSaldo() < 0, "Não existe dinheiro disponivel para saque");
     }
 
     @Test
