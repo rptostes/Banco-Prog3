30a9936b304e5b633177ffa6d7fd385b2efec237
davi-tr <davitrajano473@gmail.com>
2022-08-30 00:27:31 -0300

    Testes da classe Cliente finalizados

--- BANCO/src/test/java/br/edu/femass/model/ClienteTest.java
+++ BANCO/src/test/java/br/edu/femass/model/ClienteTest.java
@@ -1,22 +1,30 @@
 package br.edu.femass.model;
 
+import br.edu.femass.utils.Cpf;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ClienteTest {
 
     @Test
     void criarClienteCpfValido() {
-
+        Assertions.assertTrue(Cpf.isCpf("70244355002"));
     }
 
     @Test
     void criarClienteCpfInvalido() {
-
+        Assertions.assertFalse(Cpf.isCpf("70244355333"));
     }
 
     @Test
     void criarContaGerandoNumero() {
-
+        Cliente cliente = new Cliente(
+                "Rubens Silva",
+                "59996483002" );
+        cliente.criarConta(1000D);
+        Conta conta = cliente.getContas().get(0);
+        Long esperado = 1L;
+        Assertions.assertEquals(esperado, conta.getNumero());
     }
 
 
